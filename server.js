90feeafb31b68faeda0f5186044840ddc863a086
bfrangi <bernat.frangi@gmail.com>
2023-07-01 23:37:52 +0100

    First version of functional app
    Can introduce country and zip code and they are stored at the endpoint, along with feelings.

--- server.js
+++ server.js
@@ -1,19 +1,103 @@
-// Setup empty JS object to act as endpoint for all routes
+// ------------------------ GLOBAL VARIABLES -------------------------
+const apiKey = "8652a7a57419b4daf75bb786f3cf7127&units=imperial";
+const baseURL = "http://api.openweathermap.org/data/2.5/weather?";
+
+// ------------------------- SETUP ENDPOINT --------------------------
+
 projectData = {};
+// projectData = {
+//     temperature: 0,
+//     date: '',
+//     journalContent: '',
+// };
 
-// Require Express to run server and routes
+// ---------------------- SETUP EXPRESS SERVER -----------------------
 
+// Express to run server and routes
+const express = require("express");
 // Start up an instance of app
+const app = express();
 
-/* Middleware*/
-//Here we are configuring express to use body-parser as middle-ware.
+// ----------------------- SETUP DEPENDENCIES ------------------------
+
+// Configure express to use body-parser as middle-ware.
+const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require("cors");
+app.use(cors());
 
-// Initialize the main project folder
-app.use(express.static('website'));
+// ------------------------- SETUP API CALLS -------------------------
 
+async function callWeatherAPI(locationData) {
+  const url = `${baseURL}zip=${locationData.zipCode},${locationData.countryCode}&appid=${apiKey}`;
+  console.log("        Requesting data from: ", url);
+  try {
+    const response = await fetch(url);
+    const jsonData = await response.json();
+    if (jsonData.cod != 200) {
+      console.log("        Error: ", jsonData.message);
+      return { error: jsonData.message };
+    }
+    const weatherData = {
+      temperature: jsonData.main.temp,
+      location: jsonData.name,
+      weather:
+        jsonData.weather[0].main + ": " + jsonData.weather[0].description,
+    };
+    console.log("        Obtained response from OpenWeatherAPI: ", weatherData);
+    return weatherData;
+  } catch (error) {
+    console.log("        Error: ", error);
+    return { error: error };
+  }
+}
 
-// Setup Server
+// -------------------------- SETUP ROUTES ---------------------------
+
+// Route to get the current weather journal data
+app.get("/api/currentJournal", function (req, res) {
+  console.log("GET /currentJournal");
+  res.send(projectData);
+  console.log("    Sent project data: ", projectData);
+});
+
+// Route to add a new weather journal entry
+app.post("/api/addJournal", function (req, res) {
+  console.log("POST /api/addJournal");
+  projectData = {
+    weatherData: req.body.weatherData,
+    date: req.body.date,
+    journalContent: req.body.journalContent,
+  };
+  console.log("    Updated project data: ", projectData);
+  res.send(JSON.stringify("Journal added successfully!"));
+});
+
+// Route to get the current weather data
+app.post("/api/weather", async function (req, res) {
+  console.log("POST /api/weather");
+  locationData = {
+    zipCode: req.body.zipCode,
+    countryCode: req.body.countryCode,
+  };
+  console.log("    Getting data for location: " + JSON.stringify(locationData));
+  const weatherData = await callWeatherAPI(locationData);
+  res.send(weatherData);
+  console.log("    Sent data from OpenWeatherAPI to client: ", weatherData);
+});
+
+// --------------------------- RUN SERVER ----------------------------
+
+/* Initializing the main project folder */
+app.use(express.static("website"));
+
+// Spin up the server
+const port = 3000;
+const server = app.listen(port, listening);
+
+function listening() {
+  console.log(`Server running on localhost:${port}`);
+}
--- website/app.js
+++ website/app.js
@@ -1,5 +1,74 @@
-/* Global Variables */
+async function retrieveJournalGET() {
+  const response = await fetch("/api/currentJournal", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
 
-// Create a new date instance dynamically with JS
-let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+  try {
+    const newData = await response.json();
+    console.log("Current Journal:", newData);
+    return newData;
+  } catch (error) {
+    console.log("error", error);
+  }
+}
+
+async function addJournalPOST(locationData, journalContent) {
+  retrieveWeatherPOST(locationData).then(async function (weatherData) {
+    if (weatherData.error) {
+      alert("Error: " + weatherData.error);
+      return;
+    }
+    let d = new Date();
+    let newDate = d.getMonth() + "." + d.getDate() + "." + d.getFullYear();
+    const data = {
+      weatherData: weatherData,
+      date: newDate,
+      journalContent: journalContent,
+    };
+
+    const response = await fetch("/api/addJournal", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+
+    try {
+      const newData = await response.json();
+      alert(newData);
+      return newData;
+    } catch (error) {
+      console.log("error", error);
+    }
+  });
+}
+
+async function retrieveWeatherPOST(locationData) {
+  const data = locationData;
+
+  const response = await fetch("/api/weather", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+
+  try {
+    const newData = await response.json();
+    console.log("Weather: ", newData);
+    return newData;
+  } catch (error) {
+    console.log("error", error);
+  }
+}
+
+// retrieveJournalGET();
+
+// addJournalPOST({ zipCode: 28045, countryCode: "ES" }, "Today was a good day :)");
+
+// retrieveWeatherPOST();
